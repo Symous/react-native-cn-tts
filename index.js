c982f6ae1ccb91deaa9a6a1789bfab45f0b873a5
Tao.z <zhangtao@codoon.com>
2018-04-02 19:39:27 +0800

    添加speak事件监听

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 import {
   NativeModules,
+  NativeEventEmitter,
 } from 'react-native';
-export default NativeModules.CNTTS;
+// export default NativeModules.CNTTS;
+const CNTTS = NativeModules.CNTTS;
 
+class Tts extends NativeEventEmitter {
+    constructor() {
+        super(CNTTS);
+    }
+
+    init=CNTTS.init
+
+    speak (content) {
+        return CNTTS.speak(content)
+    }
+    stop () {
+        return CNTTS.stop()
+    }
+    resume () {
+        return CNTTS.resume()
+    }
+    pause () {
+        return CNTTS.pause()
+    }
+
+    addEventListener(type, handler) {
+        this.addListener(type, handler);
+    }
+
+    removeEventListener(type, handler) {
+        this.removeListener(type, handler);
+    }
+}
+
+export default new Tts()
